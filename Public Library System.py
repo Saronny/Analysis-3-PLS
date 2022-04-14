3c3d9337963837f8eec55fe1a90413547e9a7986
Saronny <101633650+Saronny@users.noreply.github.com>
2022-04-14 16:50:50 +0200

    admin
    admin login werkt nog niet member class kan nu inloggen

--- Public Library System.py
+++ Public Library System.py
@@ -4,8 +4,7 @@ import json
 import os
 
 clear = lambda: os.system('cls') # a lambda for clearing the console
-
-
+user = object    #global testing object for user 
 
 
 
@@ -35,18 +34,27 @@ class Member(Person):
         #self.borrowedbook = ?
 
     
-    def display(self):
-        print(f"{self.name} lives at {self.street_address}.")
-        print(f"email = {self.email}")
+    def display_Message(self):
+        print(f"Welcome {self.username}")
+    
+        
 
 
 class Admin(Person):
-    pass
+    def __init__(self, username, password):
 
-        
+        self.username = username
+        self.password = password
+
+    def display(self):
+        print(f"Welcome {self.username}")
+    
+
+
+
 def Login():
     # this is the login function it compares the input to the csv Member file 
-    # When login == True it will make an instance of the Person class
+    # When login_bool == True it will make an instance(user) of the Member class/ Admin class
     # username: reech1950
     # password: fgr5d4
 
@@ -55,43 +63,65 @@ def Login():
     print(""" ======PUBLIC LIBRARY=======""")
     username = input("Please enter username: ")
     password = input("Please Enter password: ")
-    with open("Members.csv", mode = "r") as f:
-            reader = csv.reader(f, delimiter = ";")
-            for row in reader:
-                if username == row[7] and password== row[8]:
-                    login_bool = True
-                    data += row
+
+    if username == "admin" and password == "admin123": #hardcoded admin login
+        login_bool == True
+    else:
+        with open("Members.csv", mode = "r") as f:
+                reader = csv.reader(f, delimiter = ";")
+                for row in reader:
+                    if username == row[7] and password== row[8]:
+                        login_bool = True
+                        data += row
                 
     if login_bool == True:
-        user = Member(username, password, data)
-        return user.display()
+        global user
+        if username == "admin" and password == "admin123":
+            user = Admin(username, password)
+            return main(2)
+        else :
+            user = Member(username, password, data)
+            clear()
+            return main(1)
     else:
         clear()
+        print("username or password incorrect")
         Login()
+
+
                 
-    
+   
 
 
 
-def main(): # Main function is for the start of the program
-    done = False
-    while done == False:
-        print(""" ======PUBLIC LIBRARY=======\n[1].Login\n[2].Exit """)
-        print(" ")
-        choice = 0
-        try:
-           choice = int(input("Please enter choice: "))
-        except:
-            print("Numbers only.")
+def main(screen = 0): # Main function is for the start of the program
+    if screen == 0: #login screen
+        login = False
+        while login == False:
+            choice = 0
+            print(""" ======PUBLIC LIBRARY=======\n[1].Login\n[2].Exit """)
+            print(" ")
+            try:
+                choice = int(input("Please enter choice: "))
+            except:
+                clear()
+                print("Numbers only.")
 
-        if choice == 1:
-            clear()
-            Login()
-            return
-        elif choice == 2:
-            sys.exit()
-        else:
-            print("Please enter correct number.")
+            if choice == 1:
+                clear()
+                return Login()
+            elif choice == 2:
+                sys.exit()
+            else:
+                print("Please enter correct number.")
+
+    if screen == 1: #Member screen
+        print(""" ======PUBLIC LIBRARY=======""")
+        user.display_Message()
+
+    if screen == 2: #Admin screen
+        print(""" ======PUBLIC LIBRARY=======""")
+        user.display_Message()
 main()
 
 

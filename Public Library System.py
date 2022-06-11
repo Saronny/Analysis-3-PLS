24ab0bf6eff9fd4060ed1d76476171d3563bdaaa
Saronny <101633650+Saronny@users.noreply.github.com>
2022-06-12 00:59:45 +0200

    Grote update
    json en csv worden aangepast met extra field. Member kan nu boek lenen en bijna returnen.(fix LoanItem class en member csv Book field! 
    wat er moet gebeuren:
                                       - LoanItem class minder fields
                                       - Book field verbeteren met meer info voor loanItem

--- Public Library System.py
+++ Public Library System.py
@@ -10,7 +10,7 @@ user = object    #global testing object for user
 
 class Book:
 
-    def __init__(self, author, country, imageLink, language, link, pages, title, isbn, year):
+    def __init__(self, author, country, imageLink, language, link, pages, title, isbn, year, bookItems):
         self.Author = author
         self.Country = country
         self.ImageLink = imageLink
@@ -20,24 +20,25 @@ class Book:
         self.Title = title
         self.Isbn = isbn
         self.Year = year
+        self.BookItems = bookItems
 
 
 class BookItem(Book): #BookItem class inherits from Book class 
 
-    def __init__(self, author, country, imageLink, language, link, pages, title, isbn, year):
-        super().__init__(author, country, imageLink, language, link, pages, title, isbn, year)
+    def __init__(self, author, country, imageLink, language, link, pages, title, isbn, year, bookItems):
+        super().__init__(author, country, imageLink, language, link, pages, title, isbn, year, bookItems)
         self.lent = False
     
 class LoanItem(BookItem): #loan item class inherits from bookItem class 
 
-    date = datetime.datetime.now()
-    return_date = date + datetime.timedelta(days=30)
+    # date = datetime.datetime.now()
+    # return_date = date + datetime.timedelta(days=30)
 
-    def __init__(self, author, country, imageLink, language, link, pages, title, isbn, year):
-        super().__init__(author, country, imageLink, language, link, pages, title, isbn, year)
-        self.BorrowedBy = object
-        self.DateLent = date.strftime("%d%m%Y")
-        self.ReturnTime = date.strftime("%d%m%Y")
+    def __init__(self, author, country, imageLink, language, link, pages, title, isbn, year, bookItems):
+        super().__init__(author, country, imageLink, language, link, pages, title, isbn, year, bookItems)
+        self.BorrowedBy = user.Username
+        self.DateLent = datetime.datetime.now()
+        self.ReturnTime = self.DateLent + datetime.timedelta(days=30)
 
     
 
@@ -50,15 +51,18 @@ class Catalog:
     def view(self):
         for book in self.List_of_books:
             print(book.Title)
+            print("Availibility: " + str(book.BookItems))
 
     def Search(self, req):
         count = 0
         for book in self.List_of_books:
             if(req in book.Title or req.capitalize() in book.Title) :
                 print(book.Author + " - " + book.Title)
+                print("Availibility: " + str(book.BookItems))
                 count += 1
             elif(req in book.Author or req.capitalize() in book.Author):
                 print(book.Author + " - " + book.Title)
+                print("Availibility: " + str(book.BookItems))
                 count += 1
         if(count == 0): 
             print("no books found.")
@@ -77,11 +81,98 @@ class Library:
     def list_books(self):
         return self.Catalog_obj.view()
         
-    def return_book():
-        pass
+    def return_book(self, bookItem):
+        item = ""
+        check = False
+        if bookItem in user.Books:
+            for books in user.Books:
+                item = books
+                check = True
+                print("You have returned: " + item)
+                break
+        else:
+            print("Invalid book")
+                    
+        if(check == True):
+            new_data = []    
+            with open ("Books.json", mode= "r") as f:
+                temp = json.load(f)
+                for book in temp:
+                    if(book["title"] == item):
+                        book["bookItems"] += 1
+                    new_data.append(book)
+                    
+            with open ("Books.json", mode= "w") as f:
+                json.dump(new_data, f)
+        
+            new_data = []
+            with open ("Members.csv", mode= "r") as f:
+                temp = csv.DictReader(f, delimiter= ";")
+                for members in temp:
+                    if(members["Username"]  == user.Username):
+                        temp = members["Books"].split("-")
+                        temp.remove(item)
+                        if(len(temp) > 0): 
+                            for item in temp:
+                                members["Books"] += item + "-"
+                        else:
+                            members["Books"] = "None"
+                    new_data.append(members)
+                    
+            with open ("Members.csv", mode= "w") as f:
+                fieldnames = ["Number", "GivenName", "Surname", "StreetAddress", "ZipCode", "City", "EmailAddress", "Username", "Password", "TelephoneNumber", "Books"]
+                write = csv.DictWriter(f, fieldnames=fieldnames)
+                write.writeheader()
+                write.writerows(new_data)
+                load_library()
+            	
 
-    def lend_book():
-        pass
+    def lend_book(self, bookItem):
+        items = self.Catalog_obj.List_of_books
+        check = False
+        if(len(user.Books) < 3):
+            loanItem = object
+            for book in items:
+                if(bookItem == book.Title and book.BookItems > 0):
+                    user.Books.append(LoanItem(book.Author, book.Country, book.ImageLink, book.Language, book.Link, book.Pages, book.Title, book.Isbn, book.Year, book.BookItems))
+                    check = True
+                    print("You have lend: " + book.Title)
+                    break
+        else:
+            print("Book not availible")
+        
+        if(check == True):
+            new_data = []    
+            with open ("Books.json", mode= "r") as f:
+                temp = json.load(f)
+                for book in temp:
+                    if(book["title"] == bookItem):
+                        book["bookItems"] -= 1
+                    new_data.append(book)
+                    
+            with open ("Books.json", mode= "w") as f:
+                json.dump(new_data, f)
+                
+            new_data = []
+            with open ("Members.csv", mode= "r") as f:
+                temp = csv.DictReader(f, delimiter= ";")
+                for members in temp:
+                    if(members["Username"]  == user.Username):
+                        if(members["Books"] == "None"):
+                            members["Books"] = bookItem
+                        else:
+                            members["Books"] += "-" + bookItem; 
+                    new_data.append(members)
+                    
+            with open ("Members.csv", mode= "w") as f:
+                fieldnames = ["Number", "GivenName", "Surname", "StreetAddress", "ZipCode", "City", "EmailAddress", "Username", "Password", "TelephoneNumber", "Books"]
+                write = csv.DictWriter(f, fieldnames=fieldnames)
+                write.writeheader()
+                write.writerows(new_data)
+                load_library()
+                
+                
+                
 
     def add_book(self, ):
         pass
@@ -102,12 +193,11 @@ class Library:
     def delete_members():
         pass
 
-
 class Person:
 
     #;GivenName;Surname;StreetAddress;ZipCode;City;EmailAddress;Username;Password;TelephoneNumber
 
-    def __init__(self, id, name, surname, address, zipCode, city, email, username, password, telephoneNumber):
+    def __init__(self, id, name, surname, address, zipCode, city, email, username, password, telephoneNumber, books):
 
         self.Id = id
         self.Name= name
@@ -119,6 +209,8 @@ class Person:
         self.Username = username
         self.Password = password
         self.TelephoneNumber = telephoneNumber
+        self.Books = books
+
 
     def display(self):
         print(f"Account of {self.Username}")
@@ -128,8 +220,11 @@ class Person:
 class Member(Person):
     def __intit__(self):
         super().__init__(self)
-        self.borrowedbooks = []
-
+        
+    def display_books(self):
+        for books in self.Books:
+            print(books.Title + "\n") 
+    
     
     def display_Message(self):
         print(f"Welcome {self.Username}")
@@ -150,27 +245,62 @@ class LibaryAdmin(Person):
 
 
 
+def write_json(data, filename):
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+
 def load_library(): # Function for object library
     list_of_books = []
     member_list = []
     bookitemlist = []
-    
     try:
         with open("Books.json") as f:
             book_data = json.load(f)
-            for i in book_data:
-                list_of_books.append(Book(i["author"], i["country"], i["imageLink"], i["language"], i["link"], i["pages"], i["title"], i["ISBN"], i["year"]))
+            check = False
+            for book in book_data:
+                if("bookItems" in book):
+                    list_of_books.append(Book(book["author"], book["country"], book["imageLink"], book["language"], book["link"], book["pages"], book["title"], book["ISBN"], book["year"], book["bookItems"]))
+                    check = True
+                else:
+                    book["bookItems"] = 3
+                    check = False
+                    list_of_books.append(Book(book["author"], book["country"], book["imageLink"], book["language"], book["link"], book["pages"], book["title"], book["ISBN"], book["year"], book["bookItems"]))
+                        
+        if(check == False):
+            write_json(book_data, "Books.json")        
+    
     except:
         print("Error: Books.json not found.")
 
+    
     try:
+        check = False
+        csv_list = []
         with open("Members.csv", mode = "r") as f:
-                    reader = csv.reader(f, delimiter = ";")
-                    for row in reader:
-                        member_list.append(Member(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
+            reader = csv.DictReader(f, delimiter = ";")
+            for dict in reader:
+                if("Books" in dict):
+                    check = True
+                    member_list.append(Member(dict["Number"], dict["GivenName"], dict["Surname"], dict["StreetAddress"], dict["ZipCode"], dict["City"], dict["EmailAddress"], dict["Username"], dict["Password"], dict["TelephoneNumber"], dict["Books"]))
+                else:
+                    csv_list.append(dict)
+                    check = False        
+
+        if(check == False):
+            with open("Members.csv", mode = "w") as f:
+                header = ["Number", "GivenName", "Surname", "StreetAddress", "ZipCode", "City", "EmailAddress", "Username", "Password", "TelephoneNumber", "Books"]
+                write = csv.DictWriter(f, fieldnames= header, delimiter= ";")
+                write.writeheader()
+                for dict in csv_list:
+                    dict["Books"] = "None"
+                    write.writerow(dict)
+                    member_list.append(Member(dict["Number"], dict["GivenName"], dict["Surname"], dict["StreetAddress"], dict["ZipCode"], dict["City"], dict["EmailAddress"], dict["Username"], dict["Password"], dict["TelephoneNumber"], dict["Books"]))
                         
     except:
         print("Error: Members.csv not found.")
+    
 
     return Library(list_of_books, member_list)
 
@@ -216,6 +346,7 @@ def Login():
 
 
 def main(screen = 0): # Main function is for the start of the program
+    global library_obj
 
     def Back(n):      #simple back function
         print("")
@@ -254,7 +385,8 @@ def main(screen = 0): # Main function is for the start of the program
         print("[1] Check catalog")
         print("[2] Search book")
         print("[3] Return book")
-        print("[4] Log out")
+        print("[4] Lend book")
+        print("[5] Log out")
         try:
             choice = int(input("Please enter choice: "))
         except:
@@ -279,10 +411,22 @@ def main(screen = 0): # Main function is for the start of the program
 
         elif choice == 3:
             clear()
-            print("WIP")
-            main(1)
-        
+            print(""" ======PUBLIC LIBRARY=======""")
+            user.display_books()
+            req = input("Please enter Title: ")
+            if(req != ""):
+                library_obj.return_book(req)
+                Back(1)
+            
         elif choice == 4:
+            clear()
+            print(""" ======PUBLIC LIBRARY=======""")
+            book = input("Please enter a Title: ")
+            if(book != ""):
+                library_obj.lend_book(book)
+                Back(1)
+        
+        elif choice == 5:
             clear()
             return main()
 
